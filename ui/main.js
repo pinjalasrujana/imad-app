6c4d06861be0c733a1172bbcfc05941418c27264
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-08-22 00:37:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,13 +5,13 @@ button.onclick =function(){
     request.onreadystatechange = function(){
         if(request.readystate === XMLHttpRequest.DONE){
             if(request.status === 200){
-                var counter = request.responseText
+                var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML=counter.toString();
             }
         }
          
-    }
+    };
     request.open('GET','http://http://u15pa1a04c8.imad.hasura-app.io/counter',true);
     request.send('null');
 };
