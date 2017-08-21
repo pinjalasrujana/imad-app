b7a371758d1cfea967bf0a079713c2a7e7af6c51
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-08-21 23:05:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element= document.getElementById('main-text');
 element.innerHTML="new value";
+
+var element= document.getElementById('change');
+       img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
