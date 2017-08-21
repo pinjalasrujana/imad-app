7a72f1baf85cecb2c4287c2a3664fba52603eef3
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-08-22 00:36:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var button= document.getElementById('counter');
 var counter=0;
 button.onclick =function(){
-    var request = now XMLHttpRequest();
+    var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readystate === XMLHttpRequest.DONE){
             if(request.status === 200){
