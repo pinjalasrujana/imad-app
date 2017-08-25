0ce9fbd13b96030a99e796c2808ba731e5b54080
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-08-25 21:38:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+
+var config = {
+    user:'u15pa1a04c8',
+    database:'u15pa1a04c8',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB-PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -85,6 +94,20 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+var pool =new Pool (config);
+app.get('/test-db', function (req, res) {
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+          res.status(500).send.err(toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    }); 
+});
+
+
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
