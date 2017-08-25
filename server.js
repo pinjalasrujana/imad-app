74a8015c02d642864f2179653c7e885c905ee26b
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-08-25 22:36:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,12 @@ app.get('/', function (req, res) {
 });
 
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 var pool =new Pool(config);
 app.get('/test-db', function (req, res) {
     pool.query('SELECT * FROM test', function(err, result){
@@ -108,7 +114,7 @@ app.get('/test-db', function (req, res) {
 });
 
 
-app.get('/article/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
     pool.query("SELECT * FROM article WHERE title ='" + req.params.articleName + "'", function(err, result){
         if(err){
           res.status(500).send(err.toString());
@@ -117,33 +123,24 @@ app.get('/article/:articleName', function (req, res) {
                 res.status(404).send('Article not found');
             }else{
                 var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
             }
-            res.send(JSON.stringify(result.rows));
-            res.send(createTemplate(articleData));
         }
     }); 
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-    counter = counter + 1;
-    res.send(counter.toString());
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
-
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
-
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
