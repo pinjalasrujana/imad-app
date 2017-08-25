ab410ae1f515fc2df5f03e5baa7187d1921ff392
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-08-25 21:45:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,11 +96,11 @@ app.get('/', function (req, res) {
 });
 
 
-var pool =new Pool (config);
+var pool =new Pool(config);
 app.get('/test-db', function (req, res) {
     pool.query('SELECT * FROM test', function(err, result){
         if(err){
-          res.status(500).send.err(toString());
+          res.status(500).send(err.toString());
         }else{
             res.send(JSON.stringify(result));
         }
