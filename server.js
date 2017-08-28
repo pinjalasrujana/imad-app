aa829a61265453dc2948fee0dc54fa14088a0131
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-08-28 23:12:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ function createTemplate(data){
                         <hr/>
                     <h3>${heading}</h3>
                     <div>
-                           ${date.toString()}
+                           ${date.toDateString()}
                     </div>
                     <div>
                         ${content}
