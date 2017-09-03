b13d794e3b93a2e2b09d76dadd9f9f35437ce77e
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-09-03 20:21:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,6 +124,33 @@ app.post('/create-user',function(req,res){
         }
     });
 });
+app.post('/login',function(res,req){
+    var username= req.body.username;
+    var password = req.body.password;
+    
+    pool.query('SELECT * FROM "user" WHERE username = $1',[username],function(err,result){
+         if(err){
+          res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length === 0){
+                res.send(403).send('username/password is invalid');
+            }
+            else{
+                var dbString = result.rows[0].password;
+                var salt = dbString.splict($)[2];
+                var hashedPassword =hash(password,salt);
+                if(hashedPassword === dbString){
+                    res.send('credentials correct');
+                }
+                else{
+                    res.send('username/password is invalid');
+                }
+                
+            }
+            
+        }
+    });
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
