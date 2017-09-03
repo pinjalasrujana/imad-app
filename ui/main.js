c9722869b05f7174ce667224d8099979d6187679
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-09-03 22:20:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ submit.onclick =function(){
      console.log(password);
      request.open('POST','http://http://u15pa1a04c8.imad.hasura-app.io/login',true);
      request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify({username: username,password: password}));
+    request.send(JSON.stringify({username: username, password: password}));
     
 };
 
