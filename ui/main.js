f0595e41fcdb9ae26b9a27eda7568e7beda93900
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-09-03 22:14:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,29 @@
-var button= document.getElementById('counter');
-var counter=0;
-button.onclick =function(){
+
+
+var submit = document.getElementById('submit_btn');
+submit.onclick =function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML=counter.toString();
+               alert('Logged in successfully');
+            } else if(request.status === 403){
+                alert('username/password is incorrect');
+            } else if(request.status === 500){
+                alert('something went wrong');
             }
         }
-         
+        
     };
-    request.open('GET','http://http://u15pa1a04c8.imad.hasura-app.io/counter',true);
-    request.send('null');
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+     console.log(password);
+     request.open('POST','http://http://u15pa1a04c8.imad.hasura-app.io/login',true);
+     request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username,password: password}));
+    
 };
-
 
 /*console.log('Loaded!');
 
@@ -29,4 +37,22 @@ function moveRight(){
 var img = document.getElementById('change');
        img.onclick = function(){
            var interval = setInterval(moveRight,50);
+};*/
+/*var button= document.getElementById('counter');
+var counter=0;
+button.onclick =function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+         request.open('GET','http://http://u15pa1a04c8.imad.hasura-app.io/counter',true);
+    request.send('null');
+    
+    };
+   
 };*/
