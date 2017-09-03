5d9617b8e705053154b34c771f830b38f2fb698e
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-09-03 15:42:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/hash/:input',function(req,res){
 });
 
 app.post('/create-user',function(req,res){
-    var salt = crypto.getRandomBytes(128).toString('hex');
+    var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password,salt);
     var username= req.body.username;
     var password = req.body.password;
