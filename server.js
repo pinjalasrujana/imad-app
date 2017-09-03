8b95553dd8d72a1aa4e5451f0be584a597f52379
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-09-03 20:57:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.post('/login',function(res,req){
     var username = req.body.username;
     var password = req.body.password;
     
-    pool.query('SELECT * FROM "user" WHERE username = $1',[username],function(err,result){
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err,result){
          if(err){
           res.status(500).send(err.toString());
         }else{
