a1a9032897697e151190db473d05920835490ccb
PInjala Jyothsna <15pa1a04c8@vishnu.edu.in>
2017-09-06 22:52:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto=require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user:'u15pa1a04c8',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret:'someRandomSecretValue',
+    cookie : {maxAge : 1000 * 60 * 60 * 24 * 30}
+}));
 
 
 function createTemplate(data){
@@ -151,6 +156,18 @@ app.post('/login',function(req,res){
         }
     });
 });
+
+app.get('/check-login', function (req, res) {
+    if(req.session && req.session.auth && req.session.auth.userId){
+        res.send('you are logged in' + req.session.auth.userId.toString());
+    }
+    else{
+        res.send('you are not logged in');
+    }
+    
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
